91687637c3f41586f732c46a481e353c89762aa4
unknown <kyeyunejonathan001@gmail.com>
2025-05-26 19:44:37 +0300

    feat: added validation to input dto fields for data intergrity. -feat: added the auditing management to truck createdat, createdby, updatedat and updatedby operations

--- src/main/java/app/nanaBank/dto/AccountsDTO.java
+++ src/main/java/app/nanaBank/dto/AccountsDTO.java
@@ -1,14 +1,23 @@
 package app.nanaBank.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDTO {
 
+    @NotEmpty(message = "accountNumber cannot be empty")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Account number must be 10 digits")
     private Long accountNumber;
+    @NotEmpty(message = "accountName cannot be empty")
     private String accountName;
+    @NotEmpty(message = "accountType cannot be empty")
     private String accountType;
+    @NotEmpty(message = "accountStatus cannot be empty")
     private String accountStatus;
+    @NotEmpty(message = "branchName cannot be empty")
     private String branchName;
+    @NotEmpty(message = "branchAddress cannot be empty")
     private String branchAddress;
 }
--- src/main/java/app/nanaBank/dto/CustomerDTO.java
+++ src/main/java/app/nanaBank/dto/CustomerDTO.java
@@ -1,11 +1,19 @@
 package app.nanaBank.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 @Data
 public class CustomerDTO {
-    
+    @NotEmpty(message = "Customer name cannot be empty")
+    @Size(min = 5, max = 30, message = "Customer name must be between 5 and 30 characters")
     private String name;
+    @Email(message = "Email should be valid")
     private String email;
+    @NotEmpty(message = "Customer mobileNumber cannot be empty")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
     private String phoneNumber;
     private AccountsDTO accountsDetails;
 }
